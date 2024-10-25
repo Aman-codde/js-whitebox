6deefaf086ab8a9a6eb4e21f55a00c07d689175c
aman-codde <amandeepk221@gmail.com>
2024-10-25 14:46:34 -0700

    feat: add calendar event

--- data.js
+++ data.js
@@ -35,26 +35,37 @@ function generateTimeSlot(timeCellstep,startTime,endTime){
         let second = str.substring(splitNum);
 
         timeSlot.push(first + ":"+ second);
-         val += 100;
-        
+         val += 100; 
     }
 }
 
 generateTimeSlot(schedule.timeCellStep,schedule.startTime,schedule.endTime);
 
-function getDatesOfWeek(date) {
-    const currentDayIndex = date.getDay();
+function formatDate(date){
+    
+    const d = new Date(date);
+    const formattedDate = d.toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'numeric',
+        day: 'numeric'
+      });
+      return formattedDate.replace(',','');
+}
+
+function getDatesOfWeek(weekOffset = 0, startDay = 0) {
+    const today = new Date();
+    const currentDay = today.getDay();
+    const daysToStartOfWeek = (currentDay - startDay + 7) % 7;
+    const startOfWeek = new Date(today);
+
+    startOfWeek.setDate(today.getDate() - daysToStartOfWeek + weekOffset * 7);
+
     const weekDates = [];
-  
-    // Calculate the date of the first day of the week (Sunday)
-    const firstDayOfWeek = new Date(date);
-    firstDayOfWeek.setDate(date.getDate() - currentDayIndex); 
     // Generate dates for the entire week
     for (let i = 0; i < 7; i++) {
-      const currentDate = new Date(firstDayOfWeek);
-      currentDate.setDate(firstDayOfWeek.getDate() + i);
-  
-      const formattedDate = currentDate.toLocaleDateString('en-US', {
+      const date = new Date(startOfWeek);
+      date.setDate(startOfWeek.getDate() + i);
+      const formattedDate = date.toLocaleDateString('en-US', {
         weekday: 'short',
         month: 'numeric',
         day: 'numeric'
@@ -63,8 +74,8 @@ function getDatesOfWeek(date) {
     }
     return weekDates;
   }
-  const today = new Date();
-  const weekDates = getDatesOfWeek(today);
+
+  const weekDates = getDatesOfWeek(0);
 
   function firstLastDate(){
     const firstDateOfWeek = weekDates[0].split(" ")[1].split("/");
@@ -78,25 +89,30 @@ function getDatesOfWeek(date) {
 const events = [
     {
         date: '10/20/2024',
-        startTime: '10am', 
-        endTime: '12pm',
+        startTime: '10:00', 
+        endTime: '12:00',
         event: 'August Batch: Deep Learning Online class by Syed@10:00 AM'
+    },
+    {
+        date: '10/22/2024',
+        startTime: '11:00', 
+        endTime: '12:00',
+        event: 'August Batch: Deep Learning Online class by Ameer@11:00 AM'
+    },
+    {
+        date: '10/25/2024',
+        startTime: '13:00', 
+        endTime: '15:00',
+        event: 'August Batch: Deep Learning Online class by Ameer@11:00 AM'
     },
     
 ];
 
-// function isLeapYear(year){
-//     if( ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
-//         // it is a leap year
-//         return 29;
-//     }
-//     else {
-//         // it is not a leap year
-//         return 28;
-//     }
-// }
-
-
+function colorBook(){
+    const colors = ['c6dbda','fee1e8','fed7c3','f6eac2','ecd5e3'];
+    const random = Math.floor(Math.random() * (colors.length-1));
+    return colors[random];
+}
 
 export {daysOfWeek, months, daysInMonth, schedule, timeSlot, 
-    weekDates, firstAndLastDate, events};
+    weekDates, firstAndLastDate, events, formatDate, colorBook, getDatesOfWeek};
